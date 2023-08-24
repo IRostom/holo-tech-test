e3c131e8663998cadcff837a9744c8d735718b26
IRostom <islam.rostom@sharwa.io>
2023-08-25 01:38:20 +0300

    map repo data and icons

--- src/components/repositoryCard/index.tsx
+++ src/components/repositoryCard/index.tsx
@@ -1,34 +1,39 @@
+import { faCodeFork, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Repository } from '../../interfaces/repositories.interface';
 import './repositoryCard.scss';
 
 export const RepositoryCard = ({ repo }: { repo: Repository }) => {
   return (
     <a
-      href="https://github.com/Nexmo/nexmo-java"
+      href={repo.html_url}
       className="github-card"
-      data-github="Nexmo/nexmo-java"
+      target="_blank"
+      rel="noopener noreferrer"
     >
-      <h3>nexmo-java</h3>
-      <p>Nexmo REST API client for Java</p>
+      <h3>{repo.name}</h3>
+      <p>{repo.description}</p>
       <span className="github-card__meta">
         <span className="github-card__language-icon" style={{ color: 'green' }}>
           ●
         </span>{' '}
-        Java
+        {repo.language}
       </span>
       <span className="github-card__meta">
         <i className="fa fa-star" aria-hidden="true"></i>
-        <span data-stars>
+        <FontAwesomeIcon icon={faStar} />
+        <span>
           <i className="fa fa-spinner" aria-hidden="true">
-            84
+            {repo.stargazers_count}
           </i>
         </span>
       </span>
       <span className="github-card__meta">
         <i className="fa fa-code-fork" aria-hidden="true">
-          3
+          <FontAwesomeIcon icon={faCodeFork} />
+          {repo.forks_count}
         </i>
-        <span data-forks>
+        <span>
           <i className="fa fa-spinner" aria-hidden="true"></i>
         </span>
       </span>
